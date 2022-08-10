df77741da33ae37ce07da8615fee0ecbc69b41fd
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-08-10 15:37:40 -0300

    Component: SignInButton

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import { FaGithub } from "react-icons/fa"
+import { FiX } from 'react-icons/fi'
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+    const isUserLoggeIn = true;
+
+    return isUserLoggeIn ? (
+        <button type="button" className={styles.signInButton}>
+            <FaGithub color="#04b301" />
+            Nicollas Ryan
+            <FiX color="#737380" className={styles.closeIncon} />
+        </button>
+    ) : (
+        <button type="button" className={styles.signInButton}>
+            <FaGithub color="#eba417" />
+            Sign in with GitHub
+        </button>
+    )
+}
