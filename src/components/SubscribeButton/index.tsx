f7adc627a877b9fd340399af01ff3382e277bdb7
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-08-11 17:07:23 -0300

    Consuming Stripe API (SSR)

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss'
 
-export function SubscribeButton() {
+interface SubscribeButtonPrpos {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonPrpos) {
     return (
         <button type="button" className={styles.subscribeButton}>
             Subscribe now
