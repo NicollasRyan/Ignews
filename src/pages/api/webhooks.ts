c7b2bdbca62a61eb4f9d5b4e721c2ef751833835
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-08-19 05:43:52 -0300

    Listening for more events, with some errors. 2 time.

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,9 +24,8 @@ export const config = {
 
 const relevantEvents = new Set([
     'checkout.session.completed', 
-    'customer.subscriptions.create', 
-    'customer.subscriptions.updated' ,
-    'customer.subscriptions.deleted'
+    'customer.subscription.updated' ,
+    'customer.subscription.deleted'
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -48,15 +47,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             try {
 
                 switch(type) {
-                    case 'customer.subscriptions.create':
-                    case 'customer.subscriptions.updated':
-                    case 'customer.subscriptions.deleted':
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
                         const subscription = event.data.object as Stripe.Subscription;
 
                         await saveSubscription(
                             subscription.id,
                             subscription.customer.toString(),
-                            event.type === 'customer.subscriptions.create',
+                            false
                         )
 
                         break;
