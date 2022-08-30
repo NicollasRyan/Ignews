a774b8a9e9cdcba6cfdc3d2f4e66d1a1a64ba88a
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-08-30 13:34:22 -0300

    listening to more events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -22,7 +22,11 @@ export const config = {
     }
 }
 
-const relevantEvents = new Set(['checkout.session.completed'])
+const relevantEvents = new Set([
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.deleted'
+])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(req.method === 'POST') {
@@ -43,6 +47,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if(relevantEvents.has(type)) {
             try {
                 switch(type) {
+                    case 'customer.subscription.created':
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+                        const subscription = event.data.object as Stripe.Subscription;
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false,
+                        )
+
+                        break;    
                     case 'checkout.session.completed':
 
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
@@ -50,6 +66,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
                             checkoutSession.customer.toString(),
+                            true
                         )
 
                         break;
