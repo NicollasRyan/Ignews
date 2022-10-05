1029166505716c3b8a1b396529d6078c62bd58d4
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-10-05 14:15:24 -0300

    Generating static previews

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
+import React from "react";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import Head from "next/head";
 
-import { SubscribeButton } from '../components/SubscribeButton';
-import styles from './home.module.scss';
-import { stripe } from '../services/stripe';
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
 interface HomeProps {
   product: {
     priceId: string;
     amount: number;
-  }
+  };
 }
-
 
 export default function Home({ product }: HomeProps) {
   return (
-
     <>
       <Head>
         <title>Home | ig.news</title>
@@ -25,7 +24,9 @@ export default function Home({ product }: HomeProps) {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about <br /> the <span>React</span> word.</h1>
+          <h1>
+            News about <br /> the <span>React</span> word.
+          </h1>
           <p>
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
@@ -33,27 +34,32 @@ export default function Home({ product }: HomeProps) {
           <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding" />
+        <Image
+          src="/aqui/images/avatar.svg"
+          alt="girl coding"
+          width={176}
+          height={176}
+        />
       </main>
     </>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1LYv3GEnKha8QJxRqAzdRVDf')
+  const price = await stripe.prices.retrieve("price_1LYv3GEnKha8QJxRqAzdRVDf");
 
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
     }).format(price.unit_amount / 100),
-  }
+  };
 
   return {
     props: {
       product,
     },
     revalidate: 60 * 60 * 24, // 24 hours
-  }
-}
+  };
+};
