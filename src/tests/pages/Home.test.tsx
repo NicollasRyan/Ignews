38604b95439d466d056096f615f4ac7b11b23a9b
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-01-05 20:58:52 -0300

    Testando página Home

--- src/tests/pages/Home.test.tsx
+++ src/tests/pages/Home.test.tsx
@@ -14,7 +14,7 @@ jest.mock("next-auth/react", () => {
 describe("Home page", () => {
   it("renders correctly", () => {
     render(<Home product={{ priceId: "fake-price-id", amount: "R$10,00" }} />);
+    expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
   });
 
-  expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
 });
